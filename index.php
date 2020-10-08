d6ae2509a277eaf608e6c2659bc79d17001ec5cb
webnetkz <8gg001@gmail.com>
2020-10-08 14:01:26 +0600

    update

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Mektep</title>
+
+        <meta charset="UTF-8">
+        <meta name="theme-color" content="rgb(0, 0, 0)">
+        <meta name="author" content="">
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="robots" content="index, follow">
+
+        <link rel="shortcut icon" href="/public/img/miniLogo.png" type="image/png">
+        <link rel="apple-touch-icon" href="/public/img/logo.png">
+        <link rel="stylesheet" type="text/css" href="public/css/style.css">
+        <link rel="stylesheet" href="public/css/mobileStyle.css">
+        <!-- <link rel="manifest" href="/public/json/manifest.json">  -->
+      </head>
+
+    <body>
+
+    </body>
+</html>
+
