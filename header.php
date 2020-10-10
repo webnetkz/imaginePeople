eb691b3d2c90a25eef13556549b1a7ef3d26096c
Yaroslav <8gg001@gmail.com>
2020-10-11 00:03:28 +0600

    append animation for people word, logo

--- header.php
+++ header.php
@@ -18,6 +18,12 @@
         font-size: 2rem;
         font-weight: 600;
     }
+    .people:nth-child(1) { animation: spin 0.5s infinite; animation-delay: 0.5s;}
+    .people:nth-child(2) { animation: spin 0.5s infinite; animation-delay: 0.8s;}
+    .people:nth-child(3) { animation: spin 0.5s infinite; animation-delay: 1.1s;}
+    .people:nth-child(4) { animation: spin 0.5s infinite; animation-delay: 1.3s;}
+    .people:nth-child(5) { animation: spin 0.5s infinite; animation-delay: 1.5s;}
+    .people:nth-child(6) { animation: spin 0.5s infinite; animation-delay: 1.8s;}
     .menuItem {
         padding: 0 1rem;
         font-size: 1.2rem;
@@ -28,11 +34,19 @@
         text-shadow: 0 0 5px rgb(255, 255, 255), 0 0 15px rgb(255, 255, 255);
         letter-spacing: 5px;
     }
+    @keyframes spin {
+        50% { transform: rotateY(360deg) }
+    }
 </style>
 <header class="flex centerAI betweenJC">
     <div class="logo" onclick="location.href = '#'">
         <span class="imagine">IMAGINE</span>
-        <span class="people">PEOPLE</span>
+        <span class="people">P<span>
+        <span class="people">E</span>
+        <span class="people">O</span>
+        <span class="people">P</span>
+        <span class="people">L</span>
+        <span class="people">E</span>
     </div>
     <menu>
         <a href="#" class="menuItem">НОВОСТИ</a>
