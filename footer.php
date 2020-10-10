7b605071deed66eb240e02876c35850152608a95
webnetkz <8gg001@gmail.com>
2020-10-10 19:48:56 +0600

    update

---
+++ footer.php
@@ -0,0 +1,59 @@
+<style>
+    footer {
+        background: rgb(75, 75, 77);
+        color: white;
+        height: 50vh;
+        margin-top: -11vh;
+        padding: 12vh 20%;
+        text-align: center;
+    }
+    footer p {
+        text-shadow: none;
+        font-weight: 900;
+    }
+    .p1b1 {
+        font-weight: 300;
+    }
+    .socialIcon {
+        margin: 15px;
+        width: 30px;
+    }
+</style>
+<footer>
+    <div>
+        <img src="public/img/logo.png" alt="" style="width: 20%;">
+    </div>
+    <div class="flex row aroundJC" style="margin-top: 30px;">
+        <div class="flex TAcenter col" style="border-right: 1px solid rgb(200, 200, 200); padding-right: 65px;">
+            <p>АДРЕС</p>
+            <p class="p1b1" style="margin-top: 50px;">Республика Казахстан,</p>
+            <p class="p1b1">050040,город Алматы, </p>
+            <p class="p1b1">мкр. Коктем-, дом 50</p>
+        </div>
+        <div class="flex TAcenter col">
+            <p>СОЦИАЛЬНЫЕ СЕТИ</p>
+            <div class="flex row beetwenJC" style="margin-top: 30px">
+                <div style="background: white; border-radius: 50%; margin: 10px;">
+                    <img src="public/img/icons/youtube.png" alt="" class="socialIcon">
+                </div>
+                <div style="background: white; border-radius: 50%; margin: 10px;">
+                    <img src="public/img/icons/instagram.png" alt="" class="socialIcon">
+                </div>
+                <div style="background: white; border-radius: 50%; margin: 10px;">
+                    <img src="public/img/icons/telegram.png" alt="" class="socialIcon">
+                </div>
+            </div>
+        </div>
+        <div class="flex TAcenter col" style="border-left: 1px solid rgb(200, 200, 200); padding-left: 65px;">
+            <p>КОНТАКТЫ</p>
+            <p class="p1b1" style="margin-top: 50px;">
+                <a href="tel:+77273645330">+7 (727) 364 53 30</a>
+            </p>
+            <p class="p1b1">
+                <a href="tel:+77767575757">+7 (776) 757 57 57</a>
+            </p>
+            <p class="p1b1" style="margin-top: 20px;">info@ip-one.net</p>
+        </div>
+    </div>
+    <p style="margin-top: 20px; font-weight: 300;">© 2020</p>
+</footer>
--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-      <title>Mektep</title>
+      <title>IP</title>
       <meta charset="UTF-8">
-      <meta name="theme-color" content="rgb(0, 0, 0)">
+      <meta name="theme-color" content="rgb(255, 255, 255)">
       <meta name="author" content="">
       <meta name="description" content="">
       <meta name="keywords" content="">
@@ -14,11 +14,10 @@
       <link rel="apple-touch-icon" href="/public/img/logo.png">
       <link rel="stylesheet" type="text/css" href="public/css/style.css">
       <link rel="stylesheet" href="public/css/mobileStyle.css">
-      <!-- <link rel="manifest" href="/public/json/manifest.json">  -->
     </head>
     <body>
+      <?php require_once 'header.php'; ?>
       <div class="one">
-        <?php require_once 'header.php'; ?>
         <div class="slider1">
           <h2>ПОЧУВСТВУЙ</h2>
           <h3>СИЛУ ПРИРОДЫ</h3>
@@ -36,23 +35,153 @@
           <img src="public/img/photo/water1.webp" alt="imagine people" class="imgSlider1">
       </div>
       </div>
-      <div class="two flex">
-        <div class="twoOne flex col">
-          <div class="rowTwoOne">
-            <img src="public/img/icons/user.png" alt="user">
+      <div class="counter flex row">
+        <div class="flex col W50 H100" style="">
+          <div class="flex H30 M10p">
+            <img src="public/img/icons/user.png" alt="" style="width: 15%; margin-left: 5%; margin-right: calc(40px + 5%);">
+            <span style="font-size: 5rem; font-weight: 500; color: rgb(26, 133, 199);">
+              73000+
+              <br>
+              <span style="font-size: 1.2rem; color: rgb(75, 75, 77); font-weight: 900;" class="VAtop">ПАРТНЕРОВ ПО ВСЕМУ МИРУ</span>
+            </span>
           </div>
-          <div class="rowTwoOne">
-            <img src="public/img/icons/home.png" alt="user">
+          <div class="flex H30 M10p">
+            <img src="public/img/icons/home.png" alt="" class="W25" style="margin-right: 40px;">
+            <span style="font-size: 5rem; font-weight: 500; color: rgb(26, 133, 199);">
+              182+
+              <br>
+              <span style="font-size: 1.2rem; color: rgb(75, 75, 77); font-weight: 900;" class="VAtop">SHOW ROOMS</span>
+            </span>
           </div>
-          <div class="rowTwoOne">
-            <img src="public/img/icons/net.png" alt="user">
+          <div class="flex H30 M10p">
+            <img src="public/img/icons/net.png" alt="" class="W25" style="margin-right: 40px;">
+            <span style="font-size: 5rem; font-weight: 500; color: rgb(26, 133, 199);">
+              3+
+              <br>
+              <span style="font-size: 1.2rem; color: rgb(75, 75, 77); font-weight: 900;" class="VAtop">ПРЕДСТАВИТЕЛЬСТВ ПО ВСЕМУ МИРУ</span>
+            </span>
           </div>
         </div>
-        <div class="twoTwo">2asdfasd</div>
+        <div class="flex col W50 H100" style="background: rgba(0, 0, 0, 0.1); color: rgb(75, 75, 77); padding: 7% 3%;">
+          <div>
+            <h2 class="" style="padding-left: 30px; font-weight: 900">ОТКРОЙ ДЛЯ СЕБЯ</h2>
+            <h1 class="" style="font-size: 4rem; font-weight: 900">IMAGINE PEOPLE</h1>
+          </div>
+          <div class="flex row" style="margin-left: -60px; margin-top: 50px;">
+            <div class="TAcenter">
+              <img src="public/img/icons/heart.png" alt="" class="W50">
+              <span class="block" style="font-weight: 900; font-size: 1.4rem">ЗДОРОВЬЕ</span>
+            </div>
+            <div class="TAcenter">
+              <img src="public/img/icons/handshake.png" alt="" class="W50">
+              <span class="block" style="font-weight: 900; font-size: 1.4rem">БИЗНЕС</span>
+            </div>            
+            <div class="TAcenter">
+              <img src="public/img/icons/money.png" alt="" class="W50">
+              <span class="block" style="font-weight: 900; font-size: 1.4rem">ДОХОД</span>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="three">
-
+      <div class="products TAcenter">
+        <h1 style="color: white; font-size: 3rem; font-weight: 900; margin-top: 15vh; text-shadow: 0 0 15px black;">НАШИ ПРОДУКТЫ</h1>
+        <div class="flex row aroundJC" style="padding: 2% 20%;">
+          <div class="flex col cartProduct">
+            <div style="background: url('public/img/photo/cartBg.webp'); background-size: cover;">
+              <img src="public/img/photo/prod1.png" alt="" class="imgProd">
+            </div>
+            <div style="padding: 0 30px;">
+              <p>G-Bio</p>
+              <span>достаточно добавить несколько капель
+              water for life в обычную питьевую
+              воду, и вода станет биогенной,
+              то есть живой. </span>
+              <a href="#" class="linkBtnX">ПОДРОБНЕЕ</a>
+            </div>
+          </div>
+          <div class="flex col cartProduct">
+            <div style="background: url('public/img/photo/cartBg.webp'); background-size: cover;">
+              <img src="public/img/photo/prod1.png" alt="" class="imgProd">
+            </div> 
+            <div style="padding: 0 30px;">
+              <p>G-Bio</p>
+              <span>достаточно добавить несколько капель
+              water for life в обычную питьевую
+              воду, и вода станет биогенной,
+              то есть живой. </span>
+              <a href="#" class="linkBtnX">ПОДРОБНЕЕ</a>
+            </div>      
+          </div>
+          <div class="flex col cartProduct">
+            <div style="background: url('public/img/photo/cartBg.webp'); background-size: cover;">
+              <img src="public/img/photo/prod1.png" alt="" class="imgProd">
+            </div>
+            <div style="padding: 0 30px;">
+              <p>G-Bio</p>
+              <span>достаточно добавить несколько капель
+              water for life в обычную питьевую
+              воду, и вода станет биогенной,
+              то есть живой. </span>
+              <a href="#" class="linkBtnX">ПОДРОБНЕЕ</a>
+            </div>
+          </div>
+        </div>
       </div>
+      <div class="video" onclick='location.href = "https:\/\/google.com"'>
+        <div class="TAcenter">
+          <h6 style="font-size: 1rem">ПОСМОТРИТЕ ВИДЕОРОЛИК</h6>
+          <h1 style="font-size: 3rem">КАК МЫ РАБОТАЕМ</h1>
+          <img src="public/img/icons/play.png" alt="" style="height: 80px; margin-top: 20px;">
+        </div>
+      </div>
+      <div class="about flex row">
+        <div class="flex col W50 H100" style="padding: 10% 3%; background: rgb(75, 75, 77);">
+          <h1 class="TAright" style="color: white; font-size: 3rem; font-weight: 900;">ЧТО О НАС</h1>
+          <h1 class="TAright" style="color: white; font-size: 3rem; font-weight: 900;">ГОВОРЯТ ЛЮДИ</h1>
+          <p class="TAright" style="text-shadow: none; margin-top: 3rem;">Мы отбираем самое интересное, что есть на сегодняшний день на рынке,
+            совершенствуем, дорабатываем и делаем из этого эксклюзивное
+            товарное предложение! В дальнейшем, когда на рынке будет создан
+            стабильно растущий потребительский спрос на данный продукт,
+            компания, для обеспечения качества и объемов производимого продукта,
+            создает своё собственное производство с возможным участием
+            в акционировании партнеров компании!</p>
+        </div>
+        <div class="flex col W50 H100" style="background: url('public/img/photo/user_img2.webp'); background-size: cover; color: rgb(75, 75, 77);">
+          <div style="width: 100%; height: 100%; background: rgba(0, 0, 0, 0.5); padding: 20% 5%;">
+            <p class="TAleft" style="color: white; text-shadow: none; font-size: 1.4rem;">Гипертония – около 30 лет, сахарный диабет – около
+            20 лет (сахар 12 йод). После 3-х месяцев употребления
+            гидроплазмы «WATER FOR LIFE» повышение давления
+            стало меньше беспокоить, прибавилось энергии,
+            улучшилось настроение, сахар снизился до 7 ед.</p>
+            <p class="TAleft" style="color: white; text-shadow: none; font-weight: 900; margin-top: 6rem;">АНДРЕЙ, 48 ЛЕТ (Г. ЧЕЛЯБИНСК)</p>
+          </div>
+        </div>
+      </div>
+      <div class="contact TAcenter">
+       <p style="color: rgb(75, 75, 77); text-shadow: none; font-weight: 400; font-size: 1.4rem">легко найти</p>
+       <h1 style="font-weight: 900; font-size: 3rem">СВЯЗЬ С НАМИ</h1>
+        <div class="flex row aroundJC" style="margin-top: 40px;">
+          <div class="flex col TAcenter">
+            <img src="public/img/icons/phone.png" alt="" style="height: 80px; width: 80px; margin-bottom: 30px; margin-left: 40px;">
+              <a href="tel:+77273645330" class="" style="font-weight: 500;">+7 (727) 364 53 30</a>
+              <a href="tel:+77767575757" class="" style="font-weight: 500;">+7 (776) 757 57 57</a>
+          </div>
+          <div class="flex col TAcenter">
+            <img src="public/img/icons/letters.png" alt="" style="height: 80px; width: 80px; margin-bottom: 30px; margin-left: 40px;">
+            <p style="color: rgb(75, 75, 77); text-shadow: none; font-weight: 500;">info@ip-one.net</p>
+          </div>
+          <div class="flex col TAcenter">
+            <img src="public/img/icons/place.png" alt="" style="height: 80px; width: 80px; margin-bottom: 30px; margin-left: 40px;">
+              <p style="color: rgb(75, 75, 77); text-shadow: none; font-weight: 500;">Республика Казахстан,</p>
+              <p style="color: rgb(75, 75, 77); text-shadow: none; font-weight: 500;">050040,город Алматы,</p>
+              <p style="color: rgb(75, 75, 77); text-shadow: none; font-weight: 500;">мкр. Коктем-, дом 50</p>
+          </div>
+        </div>
+      </div>
+      <div class="map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1453.512919637556!2d76.92624907202593!3d43.229917896312074!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38836f283211e715%3A0x89563f3c0010d4b0!2z0YPQu9C40YbQsCDQn9C40YDQvtCz0L7QstCwIDUwLCDQkNC70LzQsNGC0Ys!5e0!3m2!1sru!2skz!4v1602330502203!5m2!1sru!2skz" width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+      </div>
+      <?php require_once 'footer.php'; ?>
     </body>
 </html>
 
